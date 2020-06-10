b775e97bd9fdbb4e9c8664f79bdeda0b335c63cb
Muhammad Rehan Baig <mrb5only@gmail.com>
2020-06-10 22:08:37 +0500

    Complete

---
+++ InReach/InReach/Index.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="InReach.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
+    <style>
+        body {
+            background-color: black;
+        }
+
+        .jumbotron {
+            margin-top: 10%;
+            background-color: white !important;
+            border: solid 1px;
+            border-radius: 5px;
+            width: 70%;
+        }
+
+        input[type=email], .fu {
+            max-width: 250px;
+            max-height: 50px;
+            font-family: Tahoma;
+            font-size: 18px;
+            border-radius: 2px;
+            padding: 1%;
+        }
+    </style>
+    <title>Upload File</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <center>
+            <div class="jumbotron">
+                <div class="container">
+                    <div class="alert alert-success" runat="server" id="sucessmsg">
+                        <strong>Success!</strong> File has been uploaded please see your email.
+                    </div>
+                    <div class="alert alert-danger" runat="server" id="errormsg">
+                        <strong>Failed!</strong> Error While Uploading File to Server.
+                    </div>
+                    <h3>InReach File Upload</h3>
+                    <input type="email" runat="server" id="txtemail" required="required" placeholder="user@email.com"/><br /><br />
+                    <asp:FileUpload runat="server" ID="FileUpload" CssClass="btn btn-danger fu" /><br /><br />
+                    <asp:Button ID="btnSubmit" Text="Upload File" runat="server" OnClick="btnSubmit_Click" CssClass="btn btn-success" UseSubmitBehavior="false"/>
+                </div>
+            </div>
+            </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ InReach/InReach/Index.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InReach
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        //Local Url
+        //private string basePath = "http://localhost:63125/";
+        private string basePath = "http://www.inreach.somee.com/";
+        private string link = "";
+        private MailMessage mail = new MailMessage();
+        private SmtpClient smtp = new SmtpClient();
+        private string email = "rehan.baig.bukc@gmail.com";
+        private string password = "********";
+        private string folderpath = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            sucessmsg.Visible = false;
+            errormsg.Visible = false;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            folderpath = Server.MapPath("~/Uploads/");
+            if (FileUpload.HasFile)
+            {
+                FileUpload.SaveAs(folderpath + FileUpload.FileName);
+                link = basePath + ("/Uploads/"+ FileUpload.FileName);
+                SendEmail(txtemail.Value, link);
+                sucessmsg.Visible = true;
+            }
+            else
+            {
+                errormsg.Visible = true;
+            }
+        }
+
+        public void SendEmail(string recipent, string link)
+        {
+            mail.From = new MailAddress(email);
+            mail.To.Add(new MailAddress(recipent));
+            mail.Subject = "InReach FileUpload Test";
+            mail.IsBodyHtml = true;
+            mail.Body = "<div class='container' style='padding:20px; background-color: #f1f1f1; font-family:tahoma;'> <h2> InReach File </h2> <p>Hi</p> <br/> <b>" +link+"</b> <br/> <p> Following is uploaded Attachment Link</p> </div> ";
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential(email, password);
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Send(mail);
+        }
+    }
+}
